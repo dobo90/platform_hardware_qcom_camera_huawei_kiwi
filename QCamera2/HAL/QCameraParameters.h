d8a6a0fac3ecc7800da954ec4d5240102db3d119
Sai Kumar Sanagavarapu <ssanagav@codeaurora.org>
2014-12-11 16:56:52 +0530

    QCamera2: Set low memory configurations.
    For < 512mb devices:
    1. Enable longshot by default.
    2. Set ZSL Q depth to 1.
    3. Set longshot stages to 2.
    
    Change-Id: Ie56f41a9b736bad07a2ccd6742877677b30f4f03

--- QCamera2/HAL/QCameraParameters.h
+++ QCamera2/HAL/QCameraParameters.h
@@ -682,6 +682,7 @@ public:
     inline bool isChromaFlashEnabled() {return m_bChromaFlashOn;};
     inline bool isSeeMoreEnabled() {return m_bSeeMoreOn;};
     inline bool isTruePortraitEnabled() {return m_bTruePortraitOn;};
+    inline bool isLowMemoryDevice() {return m_bIsLowMemoryDevice;};
     inline uint32_t TpMaxMetaSize() {
         return m_pCapability->true_portrait_settings_need.meta_max_size;};
     inline uint32_t TpHeaderSize() {
@@ -719,6 +720,7 @@ public:
             isChromaFlashEnabled() || isOptiZoomEnabled() || isHDREnabled() ||
             isfssrEnabled() || isMultiTouchFocusEnabled();}
     int32_t setIntEvent(cam_int_evt_params_t params);
+    uint8_t getLongshotStages();
 
 private:
     int32_t setPreviewSize(const QCameraParameters& );
@@ -973,6 +975,7 @@ private:
     int32_t mFlashDaemonValue;
     bool m_bTruePortraitOn;
     bool m_bSensorHDREnabled;             // if HDR is enabled
+    bool m_bIsLowMemoryDevice;
 };
 
 }; // namespace qcamera
