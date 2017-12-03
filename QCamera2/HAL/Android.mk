72ea106439eaf26303f903b869c28ad137cc2c03
Dobroslaw Kijowski <dobo90@gmail.com>
2017-12-03 19:06:50 +0100

    QCamera2: Update fps info via metadata to display.
    Update fps information in preview metadata so that MDP can
    know the preview fps and can configure its block to read
    at the same fps accordingly.
    
    Change-Id: I781fad56f9316a99f23c00b19c04ec76fb5f38d8

--- QCamera2/HAL/Android.mk
+++ QCamera2/HAL/Android.mk
@@ -33,6 +33,7 @@ LOCAL_C_INCLUDES := \
         $(LOCAL_PATH)/../stack/common \
         frameworks/native/include/media/openmax \
         $(call project-path-for,qcom-display)/libgralloc \
+        $(call project-path-for,qcom-display)/libqdutils \
         $(call project-path-for,qcom-media)/libstagefrighthw \
         system/media/camera/include \
         $(LOCAL_PATH)/../../mm-image-codec/qexif \
@@ -56,7 +57,7 @@ LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
 LOCAL_SHARED_LIBRARIES := libcamera_client liblog libhardware libutils libcutils libdl
-LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface
+LOCAL_SHARED_LIBRARIES += libmmcamera_interface libmmjpeg_interface libqdMetaData
 
 LOCAL_MODULE_RELATIVE_PATH    := hw
 LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
